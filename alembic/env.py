ddfa2e42aaaeae2d1d80e4c91e34d510544ada04
Abdirakhman Asset <asetabdrakhman29052006@gmail.com>
2026-08-18 18:08:27 +0500

    feat(db): setup alembic and generate initial database migration

--- alembic/env.py
+++ alembic/env.py
@@ -1,15 +1,28 @@
 import asyncio
+import os
 from logging.config import fileConfig
-
+from dotenv import load_dotenv
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
 from alembic import context
+
+load_dotenv()
+
+config = context.config
+database_url = os.getenv("DATABASE_URL")
+
+if database_url: 
+    config.set_main_option("sqlalchemy.url", database_url)
+
+
+
+from app.db.base import Base  
+target_metadata = Base.metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-config = context.config
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,7 +33,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
