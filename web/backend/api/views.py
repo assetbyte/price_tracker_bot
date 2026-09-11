405d4dc8346e04c8c8bf0e2a0cab2bb402a3227c
Abdirakhman Asset <asetabdrakhman29052006@gmail.com>
2026-09-11 09:35:17 +0500

    feat: add POST method and TrackingCreateSerializer for tracking creation

--- web/backend/api/views.py
+++ web/backend/api/views.py
@@ -1,9 +1,12 @@
+import pstats
+
 from asgiref.sync import async_to_sync
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from app.crud.tracking import get_all_active_trackings
+from app.crud.tracking import get_all_active_trackings, create_tracking
 from app.db.session import AsyncSessionLocal
-from serializers import TrackingSerializer
+from serializers import TrackingSerializer, TrackingCreateSerializer
+from rest_framework import status
 
 class TrackingListView(APIView):
 
@@ -18,3 +21,20 @@ class TrackingListView(APIView):
     serializer = TrackingSerializer(trackings, many=True)
     
     return Response(serializer.data)
+  
+  def post(self, request):
+    serializer = TrackingCreateSerializer(data=request.data)
+    if not serializer.is_valid():
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    validated_data = serializer.validated_data
+    
+    async def create():
+      async with AsyncSessionLocal() as session:
+        new_tracking = await create_tracking(session, **validated_data)
+        return new_tracking
+    new_tracking = async_to_sync(create)()
+    output_serializer = TrackingSerializer(new_tracking)
+    return Response(output_serializer.data, status=status.HTTP_201_CREATED)
+        
+      
